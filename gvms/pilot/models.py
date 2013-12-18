edefde5017063c7b483b87c9aed73d365d46fee7
Keiran Smith <affix@affix.me>
2013-12-18 05:03:12 +0000

    Add Rank, Group and Permission Classes to Pilot Model

--- gvms/pilot/models.py
+++ gvms/pilot/models.py
@@ -17,3 +17,18 @@ class Profile(models.Model):
 	transfer_hours = models.FloatField()
 	ip_address = models.CharField(max_length=100)
 	salt       = models.CharField(max_length=30)
+	groupid    = models.ManyToManyField(Group)
+	rank       = models.ManyToManyField(Rank)
+
+class Rank(models.Model):
+	rankid = models.IntegerField()
+	rank_title = models.CharField()
+
+class Permission(models.Model):
+	permissionid = models.IntegerField()
+	permission_name = models.CharField()
+	
+class Group(models.Model):
+	groupid = models.IntegerField()
+	group_name = models.CharField(max_length=30)
+	permissionid = models.ManyToManyField(Permission)
